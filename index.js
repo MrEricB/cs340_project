d8de9df278c45823f61947538185fb07f0dc09ca
Eric Biersner <eric.biersner@gmail.com>
2019-05-20 13:38:34 -0700

    added creat for employee, patient, and treatments

--- index.js
+++ index.js
@@ -63,7 +63,7 @@ app.get('/patients', function(req, res){
     test = JSON.stringify(rows);
     test = JSON.parse(test);
     context.results = test;
-    console.log(context);
+    // console.log(context);
     res.render('SHHViewAllPatients', context);
   });
 });
@@ -82,6 +82,7 @@ app.get('/patients/id=:id', function(req, res){
     test = JSON.stringify(rows);
     test = JSON.parse(test);
     context.results = test[0];
+    // console.log(context.results)
     res.render('SHHViewIndividualPatient', context)
   });
 });
@@ -118,10 +119,11 @@ app.get('/patients/treatments/id=:id&name=:name', function(req, res){
     test = JSON.parse(test);
     context.results = test;
     context.name = name;
+    context.id = id;
+    console.log(context.results);
     res.render('patientsTreatments', context);
   });
 });
-
 
 // get all doctors for a specific patient
 app.get('/patients/doctors/id=:id&name=:name', function(req, res){
@@ -138,8 +140,218 @@ app.get('/patients/doctors/id=:id&name=:name', function(req, res){
     test = JSON.parse(test);
     context.results = test;
     context.name = name;
+    context.id = id;
     res.render('patientsDoctors', context);
   });
+});
+
+/** Add treatment to patient **/
+app.get('/addTreatment/id=:id', function(req, res){
+  var id = req.params.id;
+  var sql = "SELECT * FROM Treatments" 
+  var context = {};
+  context.id = id;
+  mysql.pool.query(sql,[id], function(err, rows, feilds){
+    if(err){
+      next(err);
+      return;
+    }
+      test = JSON.stringify(rows);
+      test = JSON.parse(test);
+      context.treatments = test;
+      res.render('addTreatment', context)
+      });
+  });
+
+app.post('/addTreatment/id=:id', function(req, res){
+  console.log(req.body);
+  var pid = req.params.id;
+  var tid = req.body.treatment;
+  console.log(pid, tid);
+  var sql = "INSERT INTO `Patient_Treatment` (`Patient_ID`, `Treatment_ID`) VALUES (?,?)";
+  // insert in new treatment for patient
+  mysql.pool.query(sql,[pid, tid], function(err, rows){
+    if(err){
+      next(err);
+      return;
+    }
+    
+    res.redirect('/patients')
+  });
+
+
+
+});
+
+/** Add doctor to patient **/
+app.get("/addDoctor/id=:id", function(req, res){
+  var id = req.params.id;
+  var sql = "SELECT e.ID,e.First_Name,e.Last_Name,et.Employee_Type FROM Employees e INNER JOIN Employee_Type et ON e.Employee_Type_ID=et.ID WHERE et.Employee_Type='Doctor'"
+  var context = {};
+  context.pid = id;
+  mysql.pool.query(sql, function(err, rows, feilds){
+    if(err){
+      next(err);
+      return;
+    }
+      test = JSON.stringify(rows);
+      test = JSON.parse(test);
+      context.doctors = test;
+      // console.log(context);
+      res.render('addDoctor', context)
+      });
+});
+
+app.post('/addDoctor/id=:id', function(req, res){
+  var pid = req.body.patient;
+  var eid = req.body.doctor;
+
+  //insert into the database
+  var sql = "INSERT INTO `Doctor_Patient` (`Employee_ID`, `Patient_ID`) VALUES (?,?);";
+  mysql.pool.query(sql,[eid, pid], function(err, rows){
+    if(err){
+      next(err);
+      return;
+    }
+    res.redirect('/patients/id='+pid);
+  });
+
+});
+
+/** Adding a patient **/
+app.get('/createPatient', function(req, res){
+  var context = {}
+  //need to pass all doctors
+  var sql_1 = "SELECT * FROM Employees INNER JOIN Employee_Type ON Employees.Employee_Type_ID = Employee_Type.ID INNER JOIN Department ON Employees.Department_ID = Department.ID WHERE Employee_Type.Employee_Type='Doctor'"
+  var sql_2 = "SELECT * FROM Treatments"
+  mysql.pool.query(sql_1, function(err, rows, feilds){
+    if(err){
+      next(err);
+      return;
+    }
+    test = JSON.stringify(rows);
+    test = JSON.parse(test);
+    context.doctors = test;
+    
+    mysql.pool.query(sql_2, function(err, rows, feilds){
+      if(err){
+        next(err);
+        return;
+      }
+      test = JSON.stringify(rows);
+      test = JSON.parse(test);
+      context.treatments = test;
+      res.render('createPatient', context);
+    });
+  });
+});
+app.post('/patients', function(req, res){
+  var First_Name = req.body.firstname;
+  var Last_Name = req.body.lastname;
+  var DOB = req.body.dob;
+  var Chief_Complaint = req.body.chiefcomplaint;
+  var Insured;
+  if(req.body.insured == "Yes"){
+    Insured = '1';
+  } else {
+    Insured = '0'
+  }
+  //insert into the database
+  var sql = "INSERT INTO `Patients` (`ID`,`First_Name`, `Last_Name`, `DOB`, `Insured`, `Chief_Complaint`) VALUES (NULL,?,?,?,?,?)";
+  mysql.pool.query(sql,[First_Name,Last_Name,DOB,Insured,Chief_Complaint], function(err, rows){
+    if(err){
+      next(err);
+      return;
+    }
+    
+    res.redirect('/patients')
+  });
+
+});
+
+
+// create employee
+app.get('/createEmployee', function(req, res){
+  var context = {}
+  //need to pass all doctors
+  var sql_1 = "SELECT * FROM Employees"
+  var sql_2 = "SELECT * FROM Employee_Type"
+  var sql_3 = "SELECT * FROM Department"
+
+  mysql.pool.query(sql_1, function(err, rows, feilds){
+    if(err){
+      next(err);
+      return;
+    }
+    test = JSON.stringify(rows);
+    test = JSON.parse(test);
+    context.results = test;
+
+    mysql.pool.query(sql_2, function(err, rows, feilds){
+      if(err){
+        next(err);
+        return;
+      }
+      test = JSON.stringify(rows);
+      test = JSON.parse(test);
+      context.employee_type = test;
+
+      mysql.pool.query(sql_3, function(err, rows, feilds){
+        if(err){
+          next(err);
+          return;
+        }
+        test = JSON.stringify(rows);
+        test = JSON.parse(test);
+        context.department = test;
+        res.render('createEmployee', context);
+      });
+    });
+  });
+});
+
+app.post('/createEmployee', function(req, res){
+  var First_Name = req.body.firstname;
+  var Last_Name = req.body.lastname;
+  var Salary = req.body.salary;
+  var Department = req.body.department;
+  var Employee_Type = req.body.employee_type;
+
+  console.log(req.body);
+  //insert into the database
+  var sql = "INSERT INTO `Employees` (`First_Name`, `Last_Name`, `Salary`, `Employee_Type_ID`, `Department_ID`) VALUES (?,?,?,?,?)";
+  mysql.pool.query(sql,[First_Name,Last_Name,Salary,Department,Employee_Type], function(err, rows){
+    if(err){
+      next(err);
+      return;
+    }
+    res.redirect('/employees')
+  });
+
+});
+
+// create Treatment
+app.get('/createTreatment', function(req, res){
+  res.render('createTreatment');
+});
+
+app.post('/createTreatment', function(req, res){
+  var Treatment_Type = req.body.treatment_type;
+  var Insurance_covers = req.body.insurance_cover;
+  var Insurance_Price = req.body.insurance_price;
+  var Uninsured_Price = req.body.uninsurance_price;
+
+  console.log(req.body);
+  //insert into the database
+  var sql = "INSERT INTO `Treatments` (`Treatment_Type`, `Insurance_covers`, `Insured_Price`, `Uninsured_Price`) VALUES (?,?,?,?)";
+  mysql.pool.query(sql,[Treatment_Type, Insurance_covers, Insurance_Price, Uninsured_Price], function(err, rows){
+    if(err){
+      next(err);
+      return;
+    }
+    res.redirect('/treatments');
+  });
+
 });
 
 
@@ -163,6 +375,21 @@ app.get('/employee_types', function(req, res){
 
 //Don't know if we need this?
 // get all patient_treaments
+app.get('/employee_types', function(req, res){
+  var context = {};
+  var sql = "SELECT * FROM Patient_Treatment"
+  mysql.pool.query(sql, function(err, rows, feilds){
+    if(err){
+      next(err);
+      return;
+    }
+    test = JSON.stringify(rows);
+    test = JSON.parse(test);
+    context.results = test;
+    console.log(context);
+    res.render('SHHViewAllEmployeeType', context); 
+  });
+});
 
 //Don't know if we need this?
 // get all doctor_patients
