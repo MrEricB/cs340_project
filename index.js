25c83c6d8f1fc29f76a31d03ce8ebe15ae73727a
Kitch Kelly <kitchmkelly@gmail.com>
2019-05-20 16:43:39 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,25 +1,191 @@
 var express = require('express');
-var handlebars = require('express-handlebars').create({defaultLayout: 'main'});
-var bodyParser = require('body-parser');
-var app = express();
-
-app.use(bodyParser.urlencoded({extended:false}));
-app.use(bodyParser.json());
-
-// Connect to database
 var mysql = require('./db_info.js');
+var bodyParser = require('body-parser');
+
+var app = express();
+var handlebars = require('express-handlebars').create({defaultLayout: 'main'});
+var path = require('path');
+var router = express.router();
 
 app.engine('handlebars', handlebars.engine);
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+app.use('/static', express.static('public'));
 app.set('view engine', 'handlebars');
-app.set('port',3000);
-
+app.set('port', process.argv[2]);
+app.set('mysql', mysql);
+app.use('/', express.static('public'));
 app.use(express.static(__dirname + '/public'));
+const publicPath = path.join(__dirname, '../public');
+app.use('/', express.static(publicPath));
 
 // Home page
 app.get('/', function(req,res){
   res.render('home')
 });
+
+//function request for all patient info
+function getPatients(res, mysql, context){
+  var query = "SELECT Patient.First_Name, Patient.Last_Name, Patient.ID, Patient.Chief_Complaint FROM Patients";
+  mysql.pool.query(query, function(error, results, fields){
+      if(error){
+          res.write(JSON.stringify(error));
+          res.end();
+      }
+      context.patients = results;
+  });
+}
+
+//function request for individual patient's info
+function getIndividualPatient(req, res, mysql, context){
+  var query = "SELECT * FROM Patients WHERE Patients.ID = ?";
+  var inserts = [req.params.ID];
+  mysql.pool.query(query, inserts, function(error, results, fields){
+    if (error){
+      res.write(JSON.stringify(error));
+      res.end();
+    }
+    context.patient = results;
+    complete();
+  })
+}
+
+//function request for all employee info
+function getEmployees(res, mysql, context){
+  var query = "SELECT Employees.First_Name, Employees.Last_Name, Employees.ID, Department.Department_Name AS Department_Name FROM Employees INNER JOIN Department ON Employees.Department_ID = Department.ID";
+  mysql.pool.query(query, function(error, results, fields){
+    if(error){
+      res.write(JSON.stringify(error));
+      res.end();
+    }
+    context.employees = results;
+  })
+}
+
+//function request for individual employee's info
+function getIndividualEmployee(req, res, mysql, context){
+  var query = "SELECT Employees.First_Name, Employees.Last_Name, Department.Department_Name AS Department_Name, Employee.Employee_Type, Employee.Salary FROM Employees INNER JOIN Department ON Employees.Department_ID = Department.ID WHERE Employees.ID = ?";
+  var inserts = [req.params.ID];
+  mysql.pool.query(query, inserts, function(error, results, fields){
+    if(error){
+      res.write(JSON.stringify(error));
+      res.end();
+    }
+    context.employee = results;
+    complete();
+  })
+}
+
+//function request for an individual patients's treatment info
+function getPatientTreatments(res, mysql, context){
+  var query = "SELECT Treatments.Treatment_Type, Treatments.ID FROM Patients INNER JOIN Patinet_Treatment ON Patients.ID = Patient_Treatment.ID INNER JOIN Treatments ON Patient_Treatment.ID = Treatments.ID WHERE Patients.ID = ?";
+  var inserts = [req.params.ID];
+  mysql.pool.query(query, inserts, function(error, results, fields){
+    if(error){
+      res.write(JSON.stringify(error));
+      res.end();
+    }
+    context.treatments = results;
+    complete();
+  })
+}
+
+//function request for an individual patient's doctors info
+function getPatientDoctors(res, mysql, context){
+  var query = "SELECT Employees.First_Name, Employees.Last_Name, Employees.ID FROM Patients INNER JOIN Doctor_Patient ON Patients.ID = Doctor_Patient.Patient_ID INNER JOIN Employees ON Doctor_Patient.Employee_ID = Employees.ID WHERE Patients.ID = ?";
+  var inserts = [req.params.ID];
+  mysql.pool.query(query, inserts, function(error, results, fields){
+    if(error){
+      res.write(JSON.stringify(error));
+      res.end();
+    }
+    context.doctors = results;
+    complete();
+  })
+}
+
+//function request for an individual employee's patient info
+function getEmployeePatients(res, mysql, context){
+  var query = "SELECT Patients.First_Name, Patients.Last_Name, Patients.ID FROM Employees INNER JOIN Doctor_Patient ON Employees.ID = Doctor_Patient.Employee_ID INNER JOIN Patients ON Doctor_Patient.Patient_ID = Patients.ID WHERE Employees.ID = ?";
+  var inserts = [req.params.ID];
+  mysql.pool.query(query, inserts, function(error, results, fields){
+    if(error){
+      res.write(JSON.stringify(error));
+      res.end();
+    }
+    context.patients = results;
+    complete();
+  })
+}
+
+//get request for all patient info for SHHViewAllPatients
+router.get('/patients', function(req, res){
+  var context = {};
+  var mysql = req.app.get('mysql');
+  getPatients(res, mysql, context);
+  res.render('SHHViewAllPatients', context);
+});
+
+//get request for all employee info for SHHViewAllEmployees
+router.get('/employees', function(req, res){
+  var context = {};
+  var mysql = req.app.get('mysql');
+  getEmployees(res, mysql, context);
+  res.render('SHHViewAllEmployees', context);
+});
+
+//get request for an individual patient's info for SHHViewIndividualPatient
+router.get('/patients/id=:id', function(req, res){
+  var callbackCount = 0;
+  var context = {};
+  var mysql = req.app.get('mysql');
+  getIndividualPatient(res, mysql, context, complete);
+  getPatientTreatments(res, mysql, context, complete);
+  getPatientDoctors(res, mysql, context, complete);
+  function complete(){
+    callbackCount++;
+    if(callbackCount >= 3){
+      res.render('SHHViewIndividualPatient', context);
+    }
+  }
+});
+
+//get request for an individual employee's info for SHHViewIndividualEmployee
+router.get('/employees/id=:id', function(req, res){
+  var callbackCount = 0;
+  var context = {};
+  var mysql = req.app.get('mysql');
+  getIndividualEmployee(res, mysql, context, complete);
+  getEmployeePatients(res, mysql, context, complete);
+  function complete(){
+    callbackCount++;
+    if(callbackCount >=2){
+      res.render('SHHViewIndividualEmployee', context);
+    }
+  }
+});
+
+
+/*
+router.get('/', function(req, res){
+  var callbackCount = 0;
+  var context = {};
+  context.jsscripts = ["deleteperson.js","filterpeople.js","searchpeople.js"];
+  var mysql = req.app.get('mysql');
+  getPeople(res, mysql, context, complete);
+  getPlanets(res, mysql, context, complete);
+  function complete(){
+      callbackCount++;
+      if(callbackCount >= 2){
+          res.render('people', context);
+      }
+
+  }
+});
+*/
+
 // get all employees
+/*
 app.get('/employees', function(req, res){
   var context = {};
   var sql = "SELECT * FROM Employees INNER JOIN Department ON Employees.Department_ID = Department.ID"
@@ -34,8 +200,10 @@ app.get('/employees', function(req, res){
     res.render('SHHViewAllEmployees', context);
   });
 });
+*/
+
 // get specific employee
-app.get('/allemployees/id=:id', function(req, res){
+app.get('/employees/id=:id', function(req, res){
   var id = req.params.id;
   var sql = "SELECT * FROM Employees INNER JOIN Employee_Type ON Employees.Employee_Type_ID = Employee_Type.ID INNER JOIN Department ON Employees.Department_ID = Department.ID WHERE Employees.ID=(?)"  
   var context = {};
@@ -68,6 +236,7 @@ app.get('/patients', function(req, res){
   });
 });
 
+/*
 // get specific patient
 // TODO: need to fix in mustache file where insured is 1 or 0 should be yes or no, same with paid
 app.get('/patients/id=:id', function(req, res){
@@ -85,6 +254,7 @@ app.get('/patients/id=:id', function(req, res){
     res.render('SHHViewIndividualPatient', context)
   });
 });
+*/
 
 // get all treatments
 app.get('/treatments', function(req, res){
@@ -104,6 +274,7 @@ app.get('/treatments', function(req, res){
 });
 
 // get all treatments for a specific patient
+/*
 app.get('/patients/treatments/id=:id&name=:name', function(req, res){
   var id = req.params.id;
   var name = req.params.name;
@@ -121,8 +292,9 @@ app.get('/patients/treatments/id=:id&name=:name', function(req, res){
     res.render('patientsTreatments', context);
   });
 });
+*/
 
-
+/*
 // get all doctors for a specific patient
 app.get('/patients/doctors/id=:id&name=:name', function(req, res){
   var id = req.params.id;
@@ -141,7 +313,7 @@ app.get('/patients/doctors/id=:id&name=:name', function(req, res){
     res.render('patientsDoctors', context);
   });
 });
-
+*/
 
 //Don't know if we need this?
 // get all employee_types
@@ -173,18 +345,17 @@ app.get('/employee_types', function(req, res){
 //Don't know if we need this?
 // get specific department
 
-app.use(function(req, res){
+app.use(function(req,res){
   res.status(404);
   res.render('404');
 });
 
 app.use(function(err, req, res, next){
   console.error(err.stack);
-  res.type('plain/text');
   res.status(500);
   res.render('500');
 });
 
-app.listen(app.get('port'),function(){
-  console.log("Server is running...")
+app.listen(app.get('port'), function(){
+  console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
 });
